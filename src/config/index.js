6d4d417dcc39576f7b1fc46f041336fbc4e521f9
Jason Shin <visualbbasic@gmail.com>
2018-04-16 22:45:25 +1000

    environment variable example

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+  path: path.join(__dirname, '/../../.env')
+})
--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 // Silence is golden...
+import './config';
+import Discord from 'discord.js';
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_PRIV_KEY);
