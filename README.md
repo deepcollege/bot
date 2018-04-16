c0663e70d0b791e6718376a9eefd0c8494935474
Jason Shin <visualbbasic@gmail.com>
2018-04-16 21:25:52 +1000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+<a href="https://i.imgur.com/gW21Bko.jpg"><img width="300" src="https://i.imgur.com/gW21Bko.jpg" title="source: imgur.com" /></a>
+<h3>DeepCollege Operational Assistant (friendlybot)</h3>
+</p>
+
+> Hi.
+
---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import clean from 'gulp-clean'
+import runSequence from 'run-sequence'
+import sourcemaps from 'gulp-sourcemaps'
+import path from 'path'
+
+// Copy files other than JS
+gulp.task('others', () => {
+  return gulp.src(['src/**/*', '!src/**/*.js']).pipe(gulp.dest('dist'))
+})
+
+const paths = {
+  sourceRoot: path.join(__dirname, 'src'),
+}
+
+// Babel JS files
+gulp.task('babel', () => {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('build-clean', () => {
+  return gulp
+    .src('dist')
+    .pipe(clean({ force: true }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('build', cb => {
+  return runSequence('build-clean', 'babel', 'others', cb)
+})
